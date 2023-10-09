e7b4a1b009df752af635ff225ac4ffbf04bd78e0
Lowraince <besseion@yandex.ru>
2023-10-09 17:34:23 +0300

    init:complite

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  let sumResult = [];
+
+  if(matrix === undefined){
+    return sumResult;
+  }else{
+    matrix.forEach((array, index) => {
+      if(index % 2 !== 0){
+        matrix[index].sort(function(a,b) {return b - a})
+      }
+  
+      matrix[index].forEach(arr2 => {
+        sumResult.push(arr2)
+      })
+    })
+    
+    return sumResult;
+  }
 }
